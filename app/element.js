a1e97f0b6ada7224e75550cdda656a74347c9203
Victor Nystad <vnystad@gmail.com>
2014-08-19 13:50:08 +0200

    simplified stuff

--- app/element.js
+++ app/element.js
@@ -5,19 +5,20 @@
   var proto = Object.create(HTMLButtonElement.prototype, {
     createdCallback: {
       value: function () {
-        this.readAttributes();
         // extend this prototype with the mixins prototype
         window.extend(proto, window.mixins);
-        // use the setter method to set the value of the msg property on the mixin prototype to the html-attribute of the custom element (see index.html)
-        this.msg = this.message || 'default message';
+
+        // read attributes from html element
+        this.readAttributes();
+        
         // use the sayHello method from the mixin
         this.addEventListener('click', this.sayHello);
       }
     },
     readAttributes : {
       value: function () {
-        // get the html-attribute from the custom element
-        this.message = this.getAttribute('msg');
+        // use the setter method to set the value of the msg property on the mixin prototype to the html-attribute of the custom element (see index.html)
+        this.msg = this.getAttribute('msg') || 'default message';
       }
     }
   });
