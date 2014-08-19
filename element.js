eed7d85b3993e53077a695e8ecfabd412719be0a
Victor Nystad <vnystad@gmail.com>
2014-08-19 14:34:17 +0200

    Update 2014-08-19T12:34:17.203Z

--- element.js
+++ element.js
@@ -5,24 +5,28 @@
   var proto = Object.create(HTMLButtonElement.prototype, {
     createdCallback: {
       value: function () {
-        this.readAttributes();
         // extend this prototype with the mixins prototype
         window.extend(proto, window.mixins);
-        // use the setter method to set the value of the msg property on the mixin prototype to the html-attribute of the custom element (see index.html)
-        this.msg = this.message || 'default message';
+
+        // read attributes from html element
+        this.readAttributes();
+
         // use the sayHello method from the mixin
         this.addEventListener('click', this.sayHello);
+
+        // check what’s in the proto object
+        //console.log(proto);
       }
     },
     readAttributes : {
       value: function () {
-        // get the html-attribute from the custom element
-        this.message = this.getAttribute('msg');
+        // use the setter method to set the value of the msg property on the mixin prototype to the html-attribute of the custom element (see index.html)
+        this.msg = this.getAttribute('msg') || 'default message';
       }
     }
   });
 
-  document.registerElement('mongo-button', {
+  window.MongoButton = document.registerElement('mongo-button', {
     prototype: proto,
     extends: 'button'
   });
